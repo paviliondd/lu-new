2be2e08f605cf8ec79726c31f25236a75d5860c5
Phi Long <philongg2804@gmail.com>
2026-07-07 22:30:34 +0700

    fix: /admin

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,20 +1,10 @@
 import config from "@payload-config";
-import { handleServerFunctions, RootLayout } from "@payloadcms/next/layouts";
-import type { ServerFunctionClient } from "payload";
+import { RootLayout } from "@payloadcms/next/layouts";
 import type { ReactNode } from "react";
 import { importMap } from "./importMap";
+import { serverFunction } from "./serverFunction";
 
 export { metadata } from "@payloadcms/next/layouts";
-
-const serverFunction: ServerFunctionClient = async (args) => {
-  "use server";
-
-  return handleServerFunctions({
-    ...args,
-    config,
-    importMap,
-  });
-};
 
 export default function PayloadLayout({ children }: { children: ReactNode }) {
   return (

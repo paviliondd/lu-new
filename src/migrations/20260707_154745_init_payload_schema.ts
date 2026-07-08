6bebdde08302ca239569e69daec67e7cfcd3f736
Phi Long <philongg2804@gmail.com>
2026-07-08 20:41:37 +0700

    fix: /admin

--- src/migrations/20260707_154745_init_payload_schema.ts
+++ src/migrations/20260707_154745_init_payload_schema.ts
@@ -1,4 +1,5 @@
-import { MigrateUpArgs, MigrateDownArgs, sql } from '@payloadcms/db-postgres'
+import { sql } from '@payloadcms/db-postgres'
+import type { MigrateDownArgs, MigrateUpArgs } from '@payloadcms/db-postgres'
 
 export async function up({ db }: MigrateUpArgs): Promise<void> {
   await db.execute(sql`
